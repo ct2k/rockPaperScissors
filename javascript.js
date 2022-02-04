81ca0ab5cbc769e052a1d2205484cbaf9070a366
ct2k <voicesfromthedarkside@protonmail.com>
2022-02-03 18:16:43 -0800

    Task 2 complete, Task 3 started

--- javascript.js
+++ javascript.js
@@ -1,14 +1,14 @@
 // JavaScript
 
 // Task 1 done. Function declaration.
+// Changed code during Task 2. Wasn't giving a random number anymore.
 
-let myArray = ['Rock', 'Paper', 'Scissors'];
+const myArray = ['Rock', 'Paper', 'Scissors'];
 
 function computerPlay() {
-    let randomize = myArray[Math.floor(Math.random()* myArray.length)];
-    return randomize;
+    return myArray[Math.floor(Math.random()* myArray.length)];
+    
 }
-
 
 // Function expression attempt.
 // Code does not work. 'Return not in function'.
@@ -20,18 +20,40 @@ function computerPlay() {
 //     return computerPlay;
 // }
 
+// Task 2
 
- 
+let playerSelection = 'Rock';
+let lowerCasesRock = 'Rock'.toLowerCase();
+const computerSelection = computerPlay();
 
 
-// function roundOne(playerSelection, computerSelection) {
+function roundOne(playerSelection, computerSelection) {
+        
+    if (playerSelection === 'Rock' && computerSelection === 'Paper') {
+        alert('computer wins!');
+        }    
 
+}
+
+// Task 3
+
+function game() {
+    roundOne(playerSelection, computerSelection);
+}
+
+
+// Unused code
+
+// return alert('Computer scored a point!');
+// } else if (computerSelection === 'Scissors' && playerSelection === 'Paper') {
+//     return alert('Computer scored a point')
+// }   else if (computerSelection === 'Paper' && playerSelection === 'Rock') {
+//     return alert('Computer scored a point!')
+// } 
+
+
+// else if (playerSelection === 'Paper' && computerSelection === 'Scissors') {
+//     alert('computer wins');
 // }
 
-
-
-// if (computerSelection === 'rock' && playerSelection === 'scissors') {
-//     alert('Computer scored a point!');
-//     } else if (computerSelection === 'scissors' && playerSelection === 'paper') {
-//         alert('Computer scored a point')
-//     }   
+// }
