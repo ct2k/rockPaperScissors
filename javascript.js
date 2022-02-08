fd2c00b745b359dc7a60549c17e3720807a0397f
ct2k <voicesfromthedarkside@protonmail.com>
2022-02-07 16:32:59 -0800

    Function inside function attempts.

--- javascript.js
+++ javascript.js
@@ -27,71 +27,50 @@ function computerPlay() {
 // const computerSelection moved out of global scope into function
 // console.log removed
 
-function playerPlay(playerSelection, computerSelection) {
+function game() {
 
-    computerSelection = computerPlay();
-    playerSelection = prompt('enter value').toLocaleLowerCase();
-    if (playerSelection === 'rock' && computerSelection === 'Paper') {
-        return (roundWinner = 'Paper beats Rock. Computer wins!');
-    } else if (playerSelection === 'rock' && computerSelection === 'Scissors') {
-        return (roundWinner = 'Rock beats Scissors. Player wins!');
-    } else if (playerSelection === 'paper' && computerSelection === 'Scissors') {
-        return (roundWinner = 'Scissors beat Paper. Computer wins!');
-    } else if (playerSelection === 'paper' && computerSelection === 'Rock') {
-        return (roundWinner = 'Paper beats Rock. Player wins!');
-    } else if (playerSelection === 'scissors' && computerSelection === 'Paper') {
-        return (roundWinner = 'Scissors beat Paper. Player Wins!');
-    } else if (playerSelection === 'scissors' && computerSelection === 'Rock') {
-        return (roundWinner = 'Rock beats Scissors. Computer wins!');
-    } else if (playerSelection === 'rock' && computerSelection === 'Rock') {
-        return (tieGame = 'Rock = Rock. Tie game!');
-    } else if (playerSelection === 'scissors' && computerSelection === 'Scissors') {
-        return (tieGame = 'Scissors = Scissors. Tie game!');
-    } else if (playerSelection === 'paper' && computerSelection === 'Paper') {
-        return (tieGame = 'Paper = Paper. Tie game!');
-    } else {
-        (playerSelection === '')
-        console.log('You didn\'t enter Rock, Paper, or Scissors!');
+    let score = 0;
+    let roundWinner;
+    let tieGame;
+    if (playerSelection === roundWinner) {
+        console.log(score = score++);
     }
 
+
+    function playerPlay(playerSelection, computerSelection) {
+
+        computerSelection = computerPlay();
+        playerSelection = prompt('enter value').toLocaleLowerCase();
+        if (playerSelection === 'rock' && computerSelection === 'Paper') {
+            return (roundWinner = 'Paper beats Rock. Computer wins!');
+        } else if (playerSelection === 'rock' && computerSelection === 'Scissors') {
+            return (roundWinner = 'Rock beats Scissors. Player wins!');
+        } else if (playerSelection === 'paper' && computerSelection === 'Scissors') {
+            return (roundWinner = 'Scissors beat Paper. Computer wins!');
+        } else if (playerSelection === 'paper' && computerSelection === 'Rock') {
+            return (roundWinner = 'Paper beats Rock. Player wins!');
+        } else if (playerSelection === 'scissors' && computerSelection === 'Paper') {
+            return (roundWinner = 'Scissors beat Paper. Player Wins!');
+        } else if (playerSelection === 'scissors' && computerSelection === 'Rock') {
+            return (roundWinner = 'Rock beats Scissors. Computer wins!');
+        } else if (playerSelection === 'rock' && computerSelection === 'Rock') {
+            return (tieGame = 'Rock = Rock. Tie game!');
+        } else if (playerSelection === 'scissors' && computerSelection === 'Scissors') {
+            return (tieGame = 'Scissors = Scissors. Tie game!');
+        } else if (playerSelection === 'paper' && computerSelection === 'Paper') {
+            return (tieGame = 'Paper = Paper. Tie game!');
+        } else {
+            (playerSelection === '')
+            console.log('You didn\'t enter Rock, Paper, or Scissors!');
+        }
+
+    }
 }
+
 
 // Task 3
 
 
-function game() {
-    
-let score = 0;
-let roundWinner;
-let tieGame;
-let playerSelection;
-let computerSelection;
-
-if (playerSelection === roundWinner || computerSelection === roundWinner) {
-    score++;
-    if (score === 5) {
-        alert('You win the game!')
-    }
-} else {
-    score === tieGame;
-}
-
-console.log(playerPlay());
-console.log(score);
-console.log(playerPlay());
-console.log(score);
-console.log(playerPlay());
-console.log(score);
-console.log(playerPlay());
-console.log(score);
-console.log(playerPlay());
-console.log(score);
-
-}
-
-    // if (score === 5) {
-    //     console.log('You win the game!');
-    //     }
 
 
 
